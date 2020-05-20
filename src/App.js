7cd360e769ae90d6d43764dcfed5576fea397df4
JoaoVitor <jvmelop@gmail.com>
2020-05-20 16:16:57 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <label for="usuario" value="Usuario"/>
+    <input name="usuario" id="usuario" className="usuario" placeholder="Digite o usuario" />
   );
 }
 
